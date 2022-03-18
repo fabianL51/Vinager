d53c25e0e28f1f699514a409a2a1d31a4ef65fc9
Fabian <fabianliemawan@gmail.com>
2022-03-18 20:00:42 +0100

    update cmakelist to adapt with different directory names

--- 02_process_transaction/build/cmake_install.cmake
+++ 02_process_transaction/build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/9_Vinager")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/02_process_transaction")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-9_Vinager
+Vinager
+
+MSYS2 packages needed to run:
+cmake, gcc, ninja
